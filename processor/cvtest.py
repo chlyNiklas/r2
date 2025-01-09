43b202670ee6d0014bee9dc68bab60f52c729834
schreifuchs <kontakt@schreifuchs.ch>
2025-01-09 13:31:32 +0100

    update processor

--- processor/cvtest.py
+++ processor/cvtest.py
@@ -1,6 +1,6 @@
 from collections.abc import Sequence
-import cv2
 import numpy as np
+import cv2
 from cv2.typing import MatLike
 
 
@@ -14,30 +14,32 @@ def main():
             # Display the resulting frame
             # edge = cv2.Canny(frame, 100, 100)
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to Grayscale
-            cv2.imshow("1", img)
+            # cv2.imshow("1", img)
 
             ## remove large patches
-            patches = detect_patches(img, 20)
-            cv2.imshow("4", patches)
+            patches = detect_patches(img, 40)
+            # cv2.imshow("4", patches)
             img = cv2.subtract(img, patches)
+            cv2.imshow("adsf", img)
 
-            img = cv2.GaussianBlur(img, (5, 5), 0)
-            cv2.imshow("2", img)
+            # img = cv2.GaussianBlur(img, (5, 5), 0)
+            # cv2.imshow("2", img)
 
             ret, img = cv2.threshold(
-                img, 150, 255, cv2.THRESH_BINARY_INV
+                img, 200, 255, cv2.THRESH_BINARY_INV
             )  # apply threshold
             cv2.imshow("3", img)
 
             blobs = detect_blobs(img)
             print(len(blobs))
-            frame = cv2.drawKeypoints(
-                frame,
-                blobs,
-                np.array([]),
-                (0, 0, 255),
-                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
-            )
+            # frame = cv2.drawKeypoints(
+            #     frame,
+            #     blobs,
+            #     np.array([]),
+            #     (0, 0, 255),
+            #     cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+            # )
+            frame = drawKeyPts(frame, blobs, (0, 0, 225), 2)
 
             cv2.imshow("Frame", frame)
         else:
@@ -47,9 +49,19 @@ def main():
             break
 
 
+def drawKeyPts(im, keyp, col, th):
+    for curKey in keyp:
+        x = int(curKey.pt[0])
+        y = int(curKey.pt[1])
+        size = int(curKey.size)
+        cv2.circle(im, (x, y), size * 2, col, thickness=th, lineType=8, shift=0)
+    return im
+
+
 def detect_patches(img: MatLike, size: int) -> MatLike:
     img = cv2.GaussianBlur(img, (size * 2 + 1, size * 2 + 1), 0)
     _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)  # apply threshold
+    img = cv2.dilate(img, np.ones((11, 11), np.uint8), iterations=3)
     return img
 
 
