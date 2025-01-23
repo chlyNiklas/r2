5d7f9fde6b53826ce56f8faae81229e7ac8b73fb
jaiybiyo <jbo140403@stud.gibb.ch>
2025-01-23 15:12:29 +0100

    video is larger, source selector needs to be moved under, hits not fully shown

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from kivy.core.window import Window
 from kivymd.app import MDApp
 from components.video import SourceSelector, VideoDisplay
-from components.hits import HitList, HitList
+from components.hits import HitList
 from processor.model import Detector
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.clock import Clock
@@ -19,14 +19,37 @@ class MainLayout(MDBoxLayout):
 
         Clock.schedule_interval(lambda _: d.process(), 1.0 / 60.0)
 
-        self.add_widget(VideoDisplay(d))
-        self.add_widget(SourceSelector(d))
-        self.add_widget(HitList(d))
+        video_display = VideoDisplay(d)
+        video_display.size_hint = (1, 1)
+        video_display.pos_hint = {"center_x": 1, "center_y": 0.5}
+        self.add_widget(video_display)
+
+        source_selector = SourceSelector(d)
+        source_selector.size_hint = (0.3, 1)
+        source_selector.pos_hint = {"center_x": 0, "center_y": 0.5}
+        self.add_widget(source_selector)
+
+        hit_list = HitList(d)
+        hit_list.size_hint = (0.3, 1)
+        hit_list.pos_hint = {"center_x": 0, "center_y": 0.5}
+        self.add_widget(hit_list)
+
+        #self.add_widget(VideoDisplay(d))
+        #self.add_widget(SourceSelector(d))
+        #self.add_widget(HitList(d))
 
 
 class R2App(MDApp):
     def build(self):
-        return MainLayout()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return MainLayout(
+            orientation="horizontal",
+            padding=10,
+            spacing=10,
+            size_hint=(1, 1),
+            pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
 
 
 if __name__ == "__main__":
