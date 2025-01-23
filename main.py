eceaebbc2e38712b4f5b3c450b634c1f9a7f5a78
jaiybiyo <jbo140403@stud.gibb.ch>
2025-01-23 15:19:24 +0100

    moved source selector under video, hits need to be moved into own container, and be sized together with stream container

--- main.py
+++ main.py
@@ -20,23 +20,22 @@ class MainLayout(MDBoxLayout):
         Clock.schedule_interval(lambda _: d.process(), 1.0 / 60.0)
 
         video_display = VideoDisplay(d)
-        video_display.size_hint = (1, 1)
-        video_display.pos_hint = {"center_x": 1, "center_y": 0.5}
-        self.add_widget(video_display)
+        video_display.size_hint = (0.9, 0.9)
 
         source_selector = SourceSelector(d)
-        source_selector.size_hint = (0.3, 1)
-        source_selector.pos_hint = {"center_x": 0, "center_y": 0.5}
-        self.add_widget(source_selector)
+        source_selector.size_hint = (0.1, 0.1)
+        source_selector.pos_hint = {"center_x": 0.5}
+
+        stream_layout = MDBoxLayout(orientation="vertical", size_hint=(1, 1))
+        stream_layout.add_widget(video_display)
+        stream_layout.add_widget(source_selector)
+
+        self.add_widget(stream_layout)
 
         hit_list = HitList(d)
-        hit_list.size_hint = (0.3, 1)
+        hit_list.size_hint = (1, 1)
         hit_list.pos_hint = {"center_x": 0, "center_y": 0.5}
         self.add_widget(hit_list)
-
-        #self.add_widget(VideoDisplay(d))
-        #self.add_widget(SourceSelector(d))
-        #self.add_widget(HitList(d))
 
 
 class R2App(MDApp):
@@ -45,10 +44,9 @@ class R2App(MDApp):
         self.theme_cls.primary_palette = "BlueGray"
         return MainLayout(
             orientation="horizontal",
-            padding=10,
-            spacing=10,
-            size_hint=(1, 1),
-            pos_hint={"center_x": 0.5, "center_y": 0.5}
+            padding=1,
+            spacing=1,
+            size_hint=(1, 1)
         )
 
 
